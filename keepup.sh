fbfa3d7d48070812fa464da6baae925dd0e7a982
kalle <kallistoteles@gmail.com>
2022-10-15 20:52:40 +0200

    updated logging

--- keepup.sh
+++ keepup.sh
@@ -4,8 +4,10 @@ session="$(id -u -n)_keepup"
 
 # function for executing stuff in a new tmux window and output a log
 muxa () {
+  n=$(basename $1)
 	tmux has-session -t $session || tmux new-session -d -s $session
-	tmux neww -dn $1 -t $session "$@" \; pipe-pane -t $1 "cat > $1.log"
+  pane=$(tmux neww -P -n $n -t $session "$@")
+  tmux pipe-pane -t $pane "cat >~/keepup_$n.log"
 }
 
 # syncthing ecample
@@ -14,10 +16,6 @@ muxa () {
 # exmaple of a script
 # pgrep -f script.sh || muxa ~/scripts/script.sh
 
-# exmaple of starting a process as another use
+# exmaple of starting a process as another user
 # pgrep tor || muxa /usr/bin/su -c daemon -m _tor -c '/usr/local/bin/tor'
 
-# check that the external drive is mounted
-# if ! mount | grep "/mnt/extern"; then
-#         pgrep fsck || fsck -y /dev/sd1a && mount /dev/sd1a /mnt/extern
-# fi
